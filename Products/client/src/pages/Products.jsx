3cb1cec8cfa0e3ffd8c0a471aed8cff418bb5b21
Christine Dow <johndoe@example.com>
2021-10-13 12:38:18 +0800

    Products part 2

---
+++ Products/client/src/pages/Products.jsx
@@ -0,0 +1,44 @@
+import { Helmet } from "react-helmet";
+import { Redirect, Route, Switch, useParams } from "react-router";
+import Filter from "../components/Products/Filter";
+import Gallery from "../components/Products/Gallery";
+import Heading from "../components/Products/Heading";
+import Navbar from "../assets/navbar.png";
+function Products() {
+  let { category } = useParams();
+
+  return (
+    <Switch>
+      <Route path="/products/innovations">
+        <Helmet>
+          <title>Innovations | </title>
+        </Helmet>
+        <img src={Navbar} alt="" style={{ width: "100%" }} />
+        <Heading category={category} />
+
+        <Gallery props={category} />
+      </Route>
+      <Route path="/products/books">
+        <Helmet>
+          <title>Books | </title>
+        </Helmet>
+        <img src={Navbar} alt="" style={{ width: "100%" }} />
+        <Heading category={category} />
+        <Gallery props={category} />
+      </Route>
+      <Route path="/products/souvenirs">
+        <Helmet>
+          <title>Souvenirs | </title>
+        </Helmet>
+        <img src={Navbar} alt="" style={{ width: "100%" }} />
+        <Heading category={category} />
+        <Gallery props={category} />
+      </Route>
+      <Route path="/products/">
+        <Redirect to="/products/innovations" />
+      </Route>
+    </Switch>
+  );
+}
+
+export default Products;
